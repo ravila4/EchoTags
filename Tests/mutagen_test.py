5e881e645ec4dfcf654dc6509d393ccebeffeb39
Aeneas42 <danausetcorvus@gmail.com>
2015-01-05 19:54:25 -0700

    Added method to catch ID3NoHeader errors

--- Tests/mutagen_test.py
+++ Tests/mutagen_test.py
@@ -9,11 +9,19 @@
 # notes           :
 # python_version  :3.4
 # ==============================================================================
+import mutagen
+from mutagen.id3 import ID3, TXXX, TPE1, ID3NoHeaderError
 
-from mutagen.id3 import ID3, TXXX
+# Open file
+# This method prevents an 'ID3NoHeader' error if the track has no tags.
+try:
+    audio = ID3('example.mp3')
+except ID3NoHeaderError:
+    audio = ID3()
+    audio.add(TPE1(encoding=3, text=u'Artist'))
+    audio.save('example.mp3')
 
 # Writing a custom TXX tag with the ID3 module.
-audio = ID3("example.mp3")
 audio.add(TXXX(encoding=0, desc='TRACK_ID', text=['TRQNFKP14AAEDDC9D7']))
 audio.save()
 print('Track ID:', audio.get('TXXX:TRACK_ID'))
