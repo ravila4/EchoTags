3e54298ea3df0a8ade8410134e75e0df248f2f21
Aeneas42 <danausetcorvus@gmail.com>
2015-01-06 19:43:23 -0700

    Major changes. Now uses song/search method with the audio_summary bucket to fetch attributes.

--- echotag.py
+++ echotag.py
@@ -2,77 +2,71 @@
 # project         :EchoTags
 # title           :echotag.py
 # description     :Program for tagging mp3 files with various useful attributes.
-# author          :ravila
-# date            :January 02, 2015
-# version         :
-# usage           :python3 track_upload.py path-audio audio-type
-# notes           :
+# author          :ravila4
+# date            :January 06, 2015
+# version         :2.0
+# usage           :python3 track_upload.py path-audio
+# notes           :Multiple audi files may be opened using a wildcard. Like so: *.mp3
 # python_version  :3.4
 # ==============================================================================
-# To Do:
-#  - handle multiple files with wildcards
-#  - support for other file types
-#
-# ===============================================================================
+# TODO: Add support for other file types.
+# TODO: Issue a warning when the file is not found in database.
 
 
-import pyen
 import sys
-import time
+import os
+import pyen
 from mutagen.id3 import ID3, TXXX
-
-en = pyen.Pyen('D6JNAFBHJO6UD84Z7')
-en.trace = False
+from termcolor import colored
 
 
-def wait_for_analysis(id):
-    print('Fetching track attributes...')
-    time.sleep(1)
-    while True:
-        # Make an API call for track's audio_summary and retry every second until complete.
-        response = en.get('track/profile', id=id, bucket=['audio_summary'])
-        if response['track']['status'] != 'pending':
-            break
-        time.sleep(1)
-    print('\n')
-    # Print audio_summary attributes
-    for k, v in response['track']['audio_summary'].items():
-        print("%s: %s" % (k, str(v)))
-        # Save attributes to tags
-        audio.add(TXXX(encoding=0, desc=k, text=str(v)))
+# Set an environment variable: 'ECHO_NEST_API_KEY'
+key = os.environ.get('ECHO_NEST_API_KEY')
+en = pyen.Pyen(key)  # Alternatively, enter your API key here.
+
+
+def get_attributes(a, t):
+    # Search for song in Echo Nest database
+    response = en.get('song/search', artist=a, title=t, results=1, bucket='audio_summary')
+    # If a song is found, save attributes to tags.
+    for song in response['songs']:
+        song_id = song['id']
+        audio.add(TXXX(encoding=0, desc='SONG_ID', text=song_id))
+        for k, v in song['audio_summary'].items():
+            audio.add(TXXX(encoding=0, desc=k, text=str(v)))
         audio.save()
-    print('\n Complete. All attributes saved to tags.')
+        print(colored('All attributes saved to tags.', 'green'))
 
-if len(sys.argv) > 2:
-    # Initial parameters
-    mp3 = sys.argv[1]
-    type = sys.argv[2]
 
-    # Open file and initiate ID3 tagging method
-    f = open(mp3, 'rb')
-    audio = ID3(mp3)
-    # Check if track_id exists in tags
-    if audio.get('TXXX:TRACK_ID') is None:
-        # Tag doesn't exist. Upload file and obtain ID
-        print('Fetching track ID...')
-        response = en.post('track/upload', track=f, filetype=type)
-        trid = response['track']['id']
-        print('Track id is', trid)
+if len(sys.argv) > 1:
+    x = len(sys.argv) - 1  # Number of files opened
 
-        # Saving ID tag to file
-        audio.add(TXXX(encoding=0, desc='TRACK_ID', text=trid))
-        audio.save()
-        print('\n')
-        print('ID saved to tag: track_id = ', audio.get('TXXX:TRACK_ID'))
+    for f in range(1, x):
+        # Open audio files one at a time
+        file = sys.argv[f]
+        audio = ID3(file)
+        # Read ID3 tags
+        artist = audio.get('TPE1')
+        title = audio.get('TIT2')
+        print('\nOpened file:', os.path.basename(file))
 
-    else:
-        # Tag exists, obtain value.
-        trid = audio.get('TXXX:TRACK_ID')
-        print('Track id is', trid)
+        # Exclude files if missing artist or title tags and output a red warning message.
+        if artist is None:
+            if title is None:
+                print(colored('Artist and title tags missing, skipping track...', 'red'))
+            else:
+                print(colored('Artist tag missing, skipping track...', 'red'))
 
-    # Call function to analyze track's acoustic properties
-    wait_for_analysis(trid)
+        elif title is None:
+            print(colored('Title tag missing, skipping track...', 'red'))
+
+        else:
+            # File has both artist and title tags.
+            print('%s by %s' % (title, artist))
+            get_attributes(artist, title)
+
 
 else:
+
     # Error message if wrong number of parameters are passed.
-    print("usage: python3 track_upload.py path-audio audio-type")
+    print("usage: python3 track_upload.py path-audio")
